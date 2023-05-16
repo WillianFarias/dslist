f8ea13114dde201c13751e6cb6558942e7613dcc
WillianFarias <williansousafarias@gmail.com>
2023-05-16 16:35:27 -0300

    refactor no nome da tabela game e alteracao do tamanho do campo long_description

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tbl_game")
+@Table(name = "tb_game")
 public class Game {
 
 	@Id
@@ -20,10 +20,11 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
 	private Double score;
 	private String imgUrl;
 	private String shortDescription;
+	@Column(length = 500)
 	private String longDescription;
 
 	public Game() {
@@ -35,7 +36,7 @@ public class Game {
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platform;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -92,11 +93,11 @@ public class Game {
 	}
 
 	public String getPlatform() {
-		return platform;
+		return platforms;
 	}
 
 	public void setPlatform(String platform) {
-		this.platform = platform;
+		this.platforms = platform;
 	}
 
 	public Double getScore() {
