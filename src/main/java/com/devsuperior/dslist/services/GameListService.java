6df70913e878dfcb6e008a0d51a821cea5728c48
WillianFarias <williansousafarias@gmail.com>
2023-05-23 00:15:37 -0300

    GameListService

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.repositories.GameListRepository;
@@ -13,7 +14,8 @@ public class GameListService {
 
 	@Autowired
 	GameListRepository gameListRepository;
-	
+
+	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		List<GameListDTO> gameLists = gameListRepository.findAll().stream().map(g -> new GameListDTO(g)).toList();
 		return gameLists;
