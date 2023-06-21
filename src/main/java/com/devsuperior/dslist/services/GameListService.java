9cbfb7998fac982846f007c35f7487acaa601fa6
WillianFarias <williansousafarias@gmail.com>
2023-06-21 20:59:30 -0300

    Implementacao de alteracao de possicao do game dentro da lista

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -7,17 +7,39 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
 
 	@Autowired
 	GameListRepository gameListRepository;
+	
+	@Autowired
+	GameRepository gameRepository;
 
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		List<GameListDTO> gameLists = gameListRepository.findAll().stream().map(g -> new GameListDTO(g)).toList();
 		return gameLists;
 	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection remove = list.remove(sourceIndex);
+		list.add(destinationIndex, remove);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex > destinationIndex ? sourceIndex : destinationIndex;
+		
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+	}
+	
+	
 }
